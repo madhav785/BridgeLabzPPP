6d4d793fc8d1ed1eead568045170ddfdecf9ee23
madhav785 <madhav.sharma_cs.h23@gla.ac.in>
2025-09-22 10:34:23 +0530

    Added Assignment

--- Inheritence-Assignment/Single_Inheritance/Library_Management_with_Books_and_Authors.java
+++ Inheritence-Assignment/Single_Inheritance/Library_Management_with_Books_and_Authors.java
@@ -33,4 +33,5 @@ public class Library_Management_with_Books_and_Authors {
         Author a = new Author("Effective Java", 2018, "Joshua Bloch", "Java expert and author");
         a.displayInfo();
     }
-}
+
+}//
